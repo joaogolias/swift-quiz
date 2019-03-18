722ecd2e53bd5cc69ec08d24f9e59ae6371f7ee0
João Golias <joaovgolias@gmail.com>
2019-03-18 15:32:04 -0300

    added tuples and booleans

---
+++ first-scripts/booleans.playground/Contents.swift
@@ -0,0 +1,45 @@
+
+/*:
+ ### BOOLEANS
+ Create a constant called `myAge` and set it to your age. Then, create a constant called `isTeenager` that uses Boolean logic to determine if the age denotes someone in the age range of 13 to 19.
+ */
+func isTeenager(_ age: Int) -> Bool {
+    return age > 13 && myAge < 19
+}
+let myAge = 22
+let isTeen = isTeenager(myAge)
+// TODO: Write solution here
+
+/*:
+ Create another constant called `leonardosAge` and set it to 14, the age of Leonardo in the 1984 Teenage Mutant Ninja Turtles comic. Then, create a constant called `eitherAreTeenagers` that uses Boolean logic to determine if either you or Leonardo are teenagers.
+ */
+let leonardosAge = 14
+let eitherAreTeenagers = isTeenager(leonardosAge) || isTeenager(myAge)
+// TODO: Write solution here
+
+/*:
+ Create a constant called student and set it to your name as a string. Create a constant called author and set it to "Matt Galloway", the original author of these exercises. Create a constant called `authorIsStudent` that uses string equality to determine if student and author are equal.
+ */
+let student = "Joao"
+let author = "Matt Galloway"
+let isStudent = student == author
+// TODO: Write solution here
+
+/*:
+ Create a constant called `studentBeforeAuthor` which uses string comparison to determine if student comes before author.
+ */
+let studentBeforeAuthor = student < author
+// TODO: Write solution here
+
+/*:
+ ### IF STATEMENTS AND BOOLEANS
+ You've already created a constant called "myAge" and checked to see if you are a teenager. Using that information, write an if statement to print out "Teenager" if you're a teenager, and "Not a teenager" if you're not.
+ */
+
+// TODO: Write solution here
+
+/*:
+ Create a constant called `answer` and use a ternary conditional operator to check if you are *not* a teenager and set it equal the same strings you printed in the above exercise. Then print out answer.
+ */
+
+// TODO: Write solution here
---
+++ first-scripts/tuples.playground/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+ ### TUPLES
+ 
+ Declare a constant tuple that contains three Int values followed by a String. Use this to represent a date (month, day, year) followed by an emoji or word you might associate with that day.
+ */
+
+var date = (day: 15, month: 08, year: 1996, felling: "happy")
+let dateDay = date.day, dateFelling = date.felling
+date.felling = "sad"
+
+
